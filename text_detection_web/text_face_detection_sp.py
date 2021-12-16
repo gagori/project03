611a9a906e797b6b01fee377ccc951653835277d
gagori <jungee123456789@gmail.com>
2021-12-16 13:15:43 +0900

    yolo function

--- text_detection_web/text_face_detection_sp.py
+++ text_detection_web/text_face_detection_sp.py
@@ -1,15 +1,15 @@
 import cv2
 from numpy.lib.arraysetops import isin
 import pytesseract
-import numpy as np
 import skew
 import camscanner
+import yolo
 
 
 # path 추가 (윈도우에선 시스템환경변수설정) : 바로가기?
 pytesseract.pytesseract.tesseract_cmd ='C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-file_path = 'img/driver4.jpg'
+file_path = 'static/img/id2.jpg'
 src = cv2.imread(file_path)
 
 
@@ -28,7 +28,7 @@ ret1, th1 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  # 
 
 ############################# Face Detection with Haar ###########################################################
 # cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  # harr분류기를 통해서 clf... YOLO가 성능은 더 좋음
-# results=cascade.detectMultiScale(blur, scaleFactor=1.1, minNeighbors=5, minSize=(5,5)) # 숫자 바꿔보기!! 낮추니까 얼굴을 더 잘잡았음.
+# results=cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(5,5)) # 숫자 바꿔보기!! 낮추니까 얼굴을 더 잘잡았음.
 
 # # print(results)  # face는 blur이전이 더 잘 잡으니까 gray_img를 받자.
 
@@ -38,67 +38,13 @@ ret1, th1 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  # 
 #     cv2.rectangle(img, (x,y),(x+w,y+h), (0,255,0), -1)
 #     cv2.putText(img, "haar", (x, y + 30), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 2)
 
-
-
 ############################## Face Detection with YOLO #############################################################
-# 이미지 가져오기
-height, width, channels = img.shape
-
-# Yolo 로드
-net = cv2.dnn.readNet("model/yolov3.weights", "model/yolov3.cfg")
-
-# Class names
-classes = []
-with open("model/coco.names", "r") as f:
-    classes = [line.strip() for line in f.readlines()]
-
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-colors = np.random.uniform(0, 255, size=(len(classes), 3))
-
-# Detecting objects
-blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-net.setInput(blob)
-outs = net.forward(output_layers)
-
-# 정보를 화면에 표시
-class_ids = []
-confidences = []
-boxes = []
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.1:
-            # Object detected
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
-            # 좌표
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
-            boxes.append([x, y, w, h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
-
-# NMS
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-
-font = cv2.FONT_HERSHEY_PLAIN
-for i in range(len(boxes)):
-    if i in indexes:
-        x, y, w, h = boxes[i]
-        label = str(classes[class_ids[i]])
-        color = colors[i]
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0,255,0), -1)
-        cv2.putText(img, "yolo", (x, y + 30), font, 3, (0,0,255), 2)
+img = yolo.yolo_face(img,0.6,0.3,False)
 
 ############################# Text Detection ######################################################################
 
 # config = r'--oem 3 --psm 6 outputbase digits'
-config = r'--oem 2 --psm 6 -c tessedit_char_whitelist=0123456789-.'
+config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789-.'
 boxes_num = pytesseract.pytesseract.image_to_data(th1, lang='kor+eng', config=config)
 # print(boxes_num)
 
