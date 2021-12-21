2a41dedcad08f485a415ee492c45fb678e8b03c2
gagori <jungee123456789@gmail.com>
2021-12-21 17:57:05 +0900

    ROI by regionselector

--- text_detection_web/camscanner.py
+++ text_detection_web/camscanner.py
@@ -1,5 +1,9 @@
+from sys import flags
 import cv2
 import numpy as np
+import pytesseract
+import os
+pytesseract.pytesseract.tesseract_cmd ='C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 
 # 꼭지점 찾기
@@ -16,8 +20,6 @@ def mapp(h):
     hnew[3] = h[np.argmax(diff)]
     return hnew
 
-
-file_path = "./img/id4.jpg"
 
 def scanner(img):  # deskew 된 이미지를 받게될 것.
     # img = cv2.imread(file_path)
@@ -43,19 +45,106 @@ def scanner(img):  # deskew 된 이미지를 받게될 것.
     dst = cv2.warpPerspective(img,op,(450,300)) # 원근법 적용
     
     # cv2.drawContours(img, contours, -1, (0,255,0), 4)
+    # cv2.imshow("test",img)
     cv2.drawContours(img, [approx], -1, (0,0,255), 2)
     return dst
 
-
 ## test ##
+# file_path = "static/img/im1.jpg"
 # img = cv2.imread(file_path)
 # img2 = img.copy()
 # dst = scanner(img2)
 
-# cv2.imshow("original", img)
+# # cv2.imshow("original", img)
 # cv2.imshow('Contour', img2)
 # cv2.imshow("Scanned Image", dst)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
+
+##########################################################################################################3
+# per=25
+
+# imgQ = cv2.imread("static/Config\pattern/DRIVER/2.jpg")
+# h,w,_ = imgQ.shape
+# imgQ = cv2.resize(imgQ,(w//3,h//3))  # 중간과정 확인용 resize
+
+# orb = cv2.ORB_create(1000)
+# kp1, des1 = orb.detectAndCompute(imgQ, None)
+# # imgKp1 = cv2.drawKeypoints(imgQ, kp1, None)
+
+# path = 'static/img'
+# file_list = os.listdir(path)
+# myPicList = [file for file in file_list if file.endswith(".jpg")]
+
+# print(myPicList)
+# # for i,y in enumerate(myPicList):
+# y = 'im01.jpg'
+# img = cv2.imread(path+"/"+ y)
+# img = cv2.resize(img, (w//3,h//3))
+# masked_img = cv2.bitwise_and(img, imgQ)
+# # cv2.imshow(y,img)
+# kp2, des2 = orb.detectAndCompute(img, None)
+# # id form과 매칭
+# bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+# matches = bf.match(des2,des1)
+# matches.sort(key=lambda x:x.distance)
+# good = matches[:int(len(matches)*(per/100))]  #25% best matches
+# imgMatch = cv2.drawMatches(img, kp2,imgQ,kp1,good[:100],None,flags=2)
+# imgMatch=cv2.resize(imgMatch,(w//3,h//3))
+# # cv2.imshow(y,imgMatch)   # todo : 우선 bitwise로 사각형박스를 나타내야 orb match 잘 될듯??
+
+# # 매핑
+# srcPoints = np.float32([kp2[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+# dstPoints = np.float32([kp1[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+# M,_ = cv2.findHomography(srcPoints, dstPoints, cv2.RANSAC, 5.0)
+# imgScan = cv2.warpPerspective(img,M,(w,h))
+# cv2.imshow(y, imgScan)
+# # cv2.imshow("KeyPoints from",imgKp1)
+# cv2.imshow("id form",imgQ)
+# cv2.waitKey(0)
+
+
+# ###############################################################
+
+# ## dst를 받아서 해보기
+# roi_IDCARD= [[(150, 300), (924, 444), 'text', 'name'],
+#             [(170, 488), (1014, 634), 'text', 'idnumber'], 
+#             [(138, 638), (1124, 910), 'text', 'address']]
+
+# roi_DRIVER = [[(192, 44), (465, 88), 'text', 'license number'], 
+#                 [(193, 84), (383, 113), 'text', 'name'], 
+#                 [(192, 110), (383, 140), 'text', 'id number'],
+#                 [(191, 137), (422, 205), 'text', 'address']]
+
+
+# # imgShow = cv2.imread("static/img/driver3.jpg")
+# imgShow = dst.copy()
+# imgQ = cv2.imread("static/Config\pattern/DRIVER/2.jpg")
+# h,w=imgQ.shape[:2]
+# imgShow = cv2.resize(imgShow, (w,h))
+# # imgMask = np.zeros_like(imgQ)
+# myData=[]
+# # print(f' ###################### Extracting Data from Form {j} ######################')
+# for x,r in enumerate(roi_DRIVER):
+#     # cv2.rectangle(imgMask, (r[0][0],r[0][1],r[1][0],r[1][1]), (0,255,0), cv2.FILLED)  # 마스크의 사이즈로 맞춰야함.
+#     # imgShow = cv2.addWeighted(imgShow, 0.99, imgMask, 0.1, 0)
+
+#     imgCrop = imgShow[r[0][1]:r[1][1] , r[0][0]:r[1][0]]  # h,w 순 슬라이싱
+#     cv2.imshow(str(x), imgCrop)
+
+#     if r[2] == "text":
+#         config = r'--oem 2 --psm 6 outputbase digits'
+#         # config = r'--oem 2 --psm 6 -c tessedit_char_whitelist=0123456789-.'
+#         print(f"{r[3]} : {pytesseract.image_to_string(imgCrop, lang='kor')}")
+#         myData.append(pytesseract.image_to_string(imgCrop,lang='kor'))
+#     cv2.putText(imgShow, str(myData[x]),(r[0][0],r[0][1]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255),1)
+
+
+# print(myData)
+# # cv2.imshow("mask",imgMask)
+# cv2.imshow("imgShow", imgShow)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
 
 
